91b03010f45458bfefbeb675e5426253f8a993a4
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-03-03 21:31:39 +0000

    Add files via upload

--- Server.cs
+++ Server.cs
@@ -1,35 +1,105 @@
 ﻿using System;
-using System.Linq;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
-class SimpleServer
+namespace SimpleHTTPServer
 {
-    static void Main(string[] args)
+    class Program
     {
-        string responseString = "<html><body><h1>Olá mundo!</h1></body></html>";
-        string s = "http://localhost:8080/";
-        Console.BackgroundColor = ConsoleColor.DarkBlue;
-        Console.Clear();
-        if (args.Count() > 0) s = args[0].ToString();
-        HttpListener listener = new HttpListener();
-        listener.Prefixes.Add(s); // define o endereço e a porta do servidor
-        listener.Start(); // inicia o servidor
-
-        Console.WriteLine("Servidor iniciado na porta 8080");
-
-        while (true)
+        static void Main(string[] args)
         {
-            HttpListenerContext context = listener.GetContext(); // aguarda uma requisição
+            String curdirs = Directory.GetCurrentDirectory();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.WriteLine("Servidor HTTP simples iniciado em http://localhost:8080/");
 
-            HttpListenerResponse response = context.Response;
-            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-            response.ContentLength64 = buffer.Length;
-            response.ContentType = "text/html"; // define o tipo de conteúdo como HTML
-            Console.WriteLine("**");
-            // escreve a resposta
-            response.OutputStream.Write(buffer, 0, buffer.Length);
-            response.OutputStream.Close();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Conexão estabelecida");
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                string request = reader.ReadLine();
+                
+
+                string[] tokens = request.Split(' ');
+                string path = tokens[1];
+
+                if (path == "/")
+                {
+                    path = "\\index.html";
+                }
+                string ppath = path.Replace("/", "\\");
+                string filename = curdirs + ppath;
+                Console.WriteLine(filename);
+                if  (File.Exists(filename))
+                {
+                    FileStream fileStream = new FileStream(filename, FileMode.Open);
+                    byte[] buffer = new byte[fileStream.Length];
+                    fileStream.Read(buffer, 0, (int)fileStream.Length);
+                    fileStream.Close();
+                    string contentType = GetContentType(filename);
+                    SendResponse(client, "200 OK", contentType, buffer);
+                }
+                else
+                {
+                    string response = "404 Not Found";
+                    byte[] buffer = Encoding.UTF8.GetBytes(response);
+                    SendResponse(client, response, "text/plain", buffer);
+                }
+
+                client.Close();
+                Console.WriteLine("Conexão terminada");
+            }
+        }
+
+        static void SendResponse(TcpClient client, string status, string contentType, byte[] data)
+        {
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            writer.WriteLine("HTTP/1.1 " + status);
+            writer.WriteLine("Content-Type: " + contentType);
+            writer.WriteLine("Content-Length: " + data.Length);
+            writer.WriteLine();
+            writer.Flush();
+
+            client.GetStream().Write(data, 0, data.Length);
+        }
+
+        static string GetContentType(string filename)
+        {
+            if (filename.EndsWith(".html") || filename.EndsWith(".htm"))
+            {
+                return "text/html";
+            }
+            else if (filename.EndsWith(".jpg") || filename.EndsWith(".jpeg"))
+            {
+                return "image/jpeg";
+            }
+            else if (filename.EndsWith(".png"))
+            {
+                return "image/png";
+            }
+            else if (filename.EndsWith(".gif"))
+            {
+                return "image/gif";
+            }
+            else if (filename.EndsWith(".css"))
+            {
+                return "text/css";
+            }
+            else if (filename.EndsWith(".js"))
+            {
+                return "text/javascript";
+            }
+            else
+            {
+                return "application/octet-stream";
+            }
         }
     }
 }
